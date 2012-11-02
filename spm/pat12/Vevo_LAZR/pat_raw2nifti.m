b37ab784405a3e99b949ffd66899e83efac2f21d
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2012-11-02 19:54:24 +0000

    //EGC Added comments to make things clearer

--- spm/pat12/Vevo_LAZR/pat_raw2nifti.m
+++ spm/pat12/Vevo_LAZR/pat_raw2nifti.m
@@ -1,6 +1,12 @@
 function pat_raw2nifti(fileName)
 % Converts RAW PA-Mode files from data export on the Vevo 2100 to NIfTI files in
-% order to be able to use SPM later, possibly for realignment/unwarping
+% order to be able to use SPM later, possibly for realignment/unwarping.
+% SYNTAX
+% pat_raw2nifti(fileName)
+% INPUT
+% fileName      Full file name to open (with extension .pamode)
+% OUTPUT
+% None
 %_______________________________________________________________________________
 % Copyright (C) 2011 LIOM Laboratoire d'Imagerie Optique et Moléculaire
 %                    École Polytechnique de Montréal
@@ -10,13 +16,15 @@ function pat_raw2nifti(fileName)
 [rawDataSO2, rawDataHbT, param] = pat_VsiOpenRawPa_multi(fileName);
 % Number of frames
 nFrames = size(rawDataHbT,4);
-% Creating nifti files to be able to use SPM later
+% Creating nifti file names
 [pathstr, name, ~] = fileparts(fileName);
 fnameSO2 = fullfile(pathstr,[name,'.SO2.nii']);
 fnameHbT = fullfile(pathstr,[name,'.HbT.nii']);
 % Single frame dimensions: [nDepth nWidth 1]
 dim = [param.PaNumSamples param.PaNumLines 1];
+% Data type
 dt = [spm_type('float64') spm_platform('bigend')];
+% Plane info
 pinfo = ones(3,1);
 % Affine transformation matrix: Scaling
 matScaling = eye(4);
@@ -35,6 +43,7 @@ matTranslation(2,4) = -param.PaDepthOffset;
 mat = matScaling * matRotation * matTranslation;
 
 fprintf('Creating NIfTI volume from %s...\n',fileName);
+% Creates NIfTI volume frame by frame
 for iFrames = 1:nFrames
     hdrSO2 = pat_create_vol(fnameSO2, dim, dt, pinfo, mat, iFrames,...
         squeeze(rawDataSO2(:,:,1,iFrames)));
--- spm/pat12/pat_create_vol.m
+++ spm/pat12/pat_create_vol.m
@@ -1,4 +1,31 @@
 function hdr = pat_create_vol(fname, dim, dt, pinfo, mat, n, data)
+% Create a NIfTI volume.
+% SYNTAX
+% hdr = pat_create_vol(fname, dim, dt, pinfo, mat, n, data)
+% INPUTS
+% fname     - the filename of the image.
+% dim       - the x, y and z dimensions of the volume
+% dt        - A 1x2 array.  First element is datatype (see spm_type).
+%               The second is 1 or 0 depending on the endian-ness.
+% pinfo     - plane info for each plane of the volume.
+%           	pinfo(1,:) - scale for each plane
+%               pinfo(2,:) - offset for each plane
+%               	The true voxel intensities of the jth image are given
+%                   by: val*pinfo(1,j) + pinfo(2,j)
+%               pinfo(3,:) - offset into image (in bytes).
+%               	If the size of pinfo is 3x1, then the volume is assumed
+%                   to be contiguous and each plane has the same scalefactor
+%                   and offset.
+% mat       - a 4x4 affine transformation matrix mapping from
+%           	voxel coordinates to real world coordinates.
+% n         - Slice position
+% data      - A 4-D matrix with volume time-series data
+% OUTPUT
+% hdr       - Header with image volume information.
+%_______________________________________________________________________________
+% Copyright (C) 2011 LIOM Laboratoire d'Imagerie Optique et Moléculaire
+%                    École Polytechnique de Montréal
+%_______________________________________________________________________________
 hdr = struct('fname',fname,...
     'dim', dim,...
     'dt',   dt,...
