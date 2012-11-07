54598f8907608c0636e7008615b6468bb881c641
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2012-11-07 12:42:50 +0000

    dd

--- pat_pca_run.m
+++ pat_pca_run.m
@@ -46,61 +46,97 @@ PATmat=job.PATmat;
 for scanIdx=1:size(PATmat,1)
     try
         load(PATmat{scanIdx});
-        nt_full = tiff_frames(PAT.tif_stack);
-        flims = [1,nt_full];
-        PAT.PCA.downsampling_factor=job.downsampling_factor;
-        % A corriger, voir comment identifier
-        badframes = [];
-        useframes = setdiff((flims(1):flims(2)), badframes);
-        n_lambda = length(useframes);
-        nPCs = min(job.nPCs, n_lambda);
-        PAT.PCA.nPCs=nPCs;
-        PAT.PCA.useframes=useframes;
-        
-        [pixw,pixh] = size(imread(PAT.tif_stack,1));
-        npix = pixw*pixh;
-        
-        % Create covariance matrix in spectral or spatial coordinates using the
-        % lowest dimension space since it is the same both ways
-        if n_lambda < npix
-            [covmat, mov, movm, movtm] = create_lcov(PAT.tif_stack, pixw, pixh, useframes, n_lambda, PAT.PCA.downsampling_factor);
-            covtrace = trace(covmat) / n_lambda;
-        else
-            [covmat, mov, movm, movtm] = create_xcov(PAT.tif_stack, pixw, pixh, useframes, n_lambda, PAT.PCA.downsampling_factor);
-            covtrace = trace(covmat) / npix;
-        end
-        
-        movm = reshape(movm, pixw, pixh);
-        
-        if n_lambda < npix
-            % Perform SVD on spectral covariance
-            [mixedsig, CovEvals, percentvar] = spectral_pat_svd(covmat, nPCs, n_lambda, npix);
+        for iFile=1:length(PAT.nifti_files)
+            % Read file
+            vol = spm_vol(PAT.nifti_files{iFile});
+            data = spm_read_vols(vol);
             
-            % Load the other set of principal components
-            [mixedfilters] = reload_moviedata(pixw*pixh, mov, mixedsig, CovEvals);
-        else
-            % Perform SVD on spatial components
-            [mixedfilters, CovEvals, percentvar] = spectral_pat_svd(covmat, nPCs, n_lambda, npix);
+            % Data is filled with zeros, find where they are
+            start_index =1;
+            end_index=size(data,1);
+            left_index=1;
+            right_index=size(data,2);
+            for itop=1:size(data,1)
+                if( sum(data(itop,:,1)) > 0)
+                    start_index=itop;
+                    break;
+                end
+            end
+            for ibot=size(data,1):-1:1
+                if( sum(data(ibot,:,1)) > 0)
+                    end_index=ibot;
+                    break;
+                end
+            end
+            for ileft=1:size(data,2)
+                if( sum(data(:,ileft,1),1) > 0)
+                    left_index=ileft;
+                    break;
+                end
+                        end
+            for iright=size(data,2):-1:1
+                if( sum(data(:,iright,1),1) > 0)
+                    right_index=iright;
+                    break;
+                end
+            end
+            data=log(abs(data(start_index:end_index,left_index:right_index,:)));
+            flims=[1,size(data,3)];
+            PAT.PCA.downsampling_factor=job.downsampling_factor;
+            % A corriger, voir comment identifier
+            badframes = [];
+            useframes = setdiff((flims(1):flims(2)), badframes);
+            n_lambda = length(useframes);
+            nPCs = min(job.nPCs, n_lambda);
+            PAT.PCA.nPCs=nPCs;
+            PAT.PCA.useframes=useframes;
             
-            % Load the other set of principal components
-            [mixedsig] = reload_moviedata(n_lambda, mov', mixedfilters, CovEvals);
+            pixw=size(data,1);
+            pixh=size(data,2);
+            npix = pixw*pixh;
+            
+            % Create covariance matrix in spectral or spatial coordinates using the
+            % lowest dimension space since it is the same both ways
+            if n_lambda < npix
+                [covmat, mov, movm, movtm] = create_lcov(data, pixw, pixh, useframes, n_lambda, PAT.PCA.downsampling_factor);
+                covtrace = trace(covmat) / n_lambda;
+            else
+                [covmat, mov, movm, movtm] = create_xcov(data, pixw, pixh, useframes, n_lambda, PAT.PCA.downsampling_factor);
+                covtrace = trace(covmat) / npix;
+            end
+            
+            movm = reshape(movm, pixw, pixh);
+            
+            if n_lambda < npix
+                % Perform SVD on spectral covariance
+                [mixedsig, CovEvals, percentvar] = spectral_pat_svd(covmat, nPCs, n_lambda, npix);
+                
+                % Load the other set of principal components
+                [mixedfilters] = reload_moviedata(pixw*pixh, mov, mixedsig, CovEvals);
+            else
+                % Perform SVD on spatial components
+                [mixedfilters, CovEvals, percentvar] = spectral_pat_svd(covmat, nPCs, n_lambda, npix);
+                
+                % Load the other set of principal components
+                [mixedsig] = reload_moviedata(n_lambda, mov', mixedfilters, CovEvals);
+            end
+            mixedfilters = reshape(mixedfilters, pixw,pixh,nPCs);
+            
+            firstframe_full = squeeze(data(:,:,1));
+            firstframe = firstframe_full;
+            if PAT.PCA.downsampling_factor(1)>1
+                firstframe = imresize(firstframe, size(mov(:,:,1)),'bilinear');
+            end
+            
+            %------------
+            % Save the output data
+            PAT.PCA.mixedfilters=mixedfilters;
+            PAT.PCA.CovEvals=CovEvals;
+            PAT.PCA.mixedsig=mixedsig;
+            PAT.PCA.movm = movm;
+            PAT.PCA.movtm=movtm;
+            PAT.PCA.covtrace=covtrace;
         end
-        mixedfilters = reshape(mixedfilters, pixw,pixh,nPCs);
-        
-        firstframe_full = imread(PAT.tif_stack,1);
-        firstframe = firstframe_full;
-        if PAT.PCA.downsampling_factor(1)>1
-            firstframe = imresize(firstframe, size(mov(:,:,1)),'bilinear');
-        end
-        
-        %------------
-        % Save the output data
-        PAT.PCA.mixedfilters=mixedfilters;
-        PAT.PCA.CovEvals=CovEvals;
-        PAT.PCA.mixedsig=mixedsig;
-        PAT.PCA.movm = movm;
-        PAT.PCA.movtm=movtm;
-        PAT.PCA.covtrace=covtrace;
         PAT.jobsdone.pca = 1;
         save(fullfile(PAT.output_dir, 'PAT.mat'),'PAT');
         out.PATmat{scanIdx} = PATmat{scanIdx};
@@ -113,7 +149,7 @@ end
 
 end
 
-function [covmat, mov, movm, movtm] = create_xcov(fn, pixw, pixh, useframes, nl, dsamp)
+function [covmat, mov, movm, movtm] = create_xcov(data, pixw, pixh, useframes, nl, dsamp)
 %-----------------------
 % Load movie data to compute the spatial covariance matrix
 
@@ -130,29 +166,16 @@ end
 
 if (dsamp_space==1)
     mov = zeros(pixw, pixh, nl);
-    for jjind=1:length(useframes)
-        jj = useframes(jjind);
-        mov(:,:,jjind) = imread(fn,jj);
-        if mod(jjind,500)==1
-            fprintf(' Read frame %4.0f out of %4.0f; ', jjind, nl)
-            toc
-        end
-    end
+    mov=data(:,:,useframes);
 else
-    [pixw_dsamp,pixh_dsamp] = size(imresize( imread(fn,1), 1/dsamp_space, 'bilinear' ));
+    [pixw_dsamp,pixh_dsamp] = size(imresize( squeeze(data(:,:,1)), 1/dsamp_space, 'bilinear' ));
     mov = zeros(pixw_dsamp, pixh_dsamp, nl);
     for jjind=1:length(useframes)
         jj = useframes(jjind);
-        mov(:,:,jjind) = imresize( imread(fn,jj), 1/dsamp_space, 'bilinear' );
-        if mod(jjind,500)==1
-            fprintf(' Read frame %4.0f out of %4.0f; ', jjind, nl)
-            toc
-        end
+        mov(:,:,jjind) = imresize( squeeze(data(:,:,jj)), 1/dsamp_space, 'bilinear' );
     end
 end
 
-fprintf(' Read frame %4.0f out of %4.0f; ', jjind, nl)
-toc
 mov = reshape(mov, npix, nl);
 
 % DFoF normalization of each pixel
@@ -171,7 +194,7 @@ covmat = c1 - movtm*movtm';
 clear c1
 end
 
-function [covmat, mov, movm, movtm] = create_lcov(fn, pixw, pixh, useframes, nl, dsamp)
+function [covmat, mov, movm, movtm] = create_lcov(data, pixw, pixh, useframes, nl, dsamp)
 %-----------------------
 % Load movie data to compute the temporal covariance matrix
 npix = pixw*pixh;
@@ -186,28 +209,16 @@ else
 end
 
 if (dsamp_space==1)
-    mov = zeros(pixw, pixh, nl);
-    for jjind=1:length(useframes)
-        jj = useframes(jjind);
-        mov(:,:,jjind) = imread(fn,jj);
-        if mod(jjind,500)==1
-            fprintf(' Read frame %4.0f out of %4.0f; ', jjind, nl)
-        end
-    end
+    mov=data(:,:,useframes);
 else
-    [pixw_dsamp,pixh_dsamp] = size(imresize( imread(fn,1), 1/dsamp_space, 'bilinear' ));
+    [pixw_dsamp,pixh_dsamp] = size(imresize( squeeze(data(:,:,1)), 1/dsamp_space, 'bilinear' ));
     mov = zeros(pixw_dsamp, pixh_dsamp, nl);
     for jjind=1:length(useframes)
         jj = useframes(jjind);
-        mov(:,:,jjind) = imresize( imread(fn,jj), 1/dsamp_space, 'bilinear' );
-        if mod(jjind,500)==1
-            fprintf(' Read frame %4.0f out of %4.0f; ', jjind, nl)
-            toc
-        end
+        mov(:,:,jjind) = imresize( squeeze(data(:,:,jj)), 1/dsamp_space, 'bilinear' );
     end
 end
 
-fprintf(' Read frame %4.0f out of %4.0f; ', jjind, nl)
 mov = reshape(mov, npix, nl);
 
 % DFoF normalization of each pixel
