906b6be698b3e6cd4a4d129209a7a5961f05e44e
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2013-02-05 23:31:41 +0000

    //EGC VsiOpenRawBmode8.m - Function to open RAW 8-bit B-Mode files from data export on the Vevo 2100 and read selected parameters. VsiShowRawBmode8.m - Display RAW 8-bit B-mode data. VsiOpenRawBmode32.m - Function to open RAW 8-bit B-Mode files from data export on the Vevo 2100 and read selected parameters. VsiShowRawBmode32.m -Display RAW 32-bit B-mode data.

---
+++ spm/pat12/Vevo_LAZR/VsiOpenRawBmode8.m
@@ -0,0 +1,52 @@
+% VsiOpenRawBmode8.m
+% Copyright VisualSonics 1999-2012
+% A. Needles
+% Revision: 1.0 Oct 24 2012
+% A function to open RAW 8-bit B-Mode files from data export on the Vevo 2100
+% and read selected parameters
+
+
+function [Rawdata, WidthAxis, DepthAxis] = VsiOpenRawBmode8(fnameBase, ModeName, iframe)
+
+% Set up file names
+fname = [fnameBase ModeName];
+fnameXml = [fnameBase '.xml'];
+
+% Parse the XML parameter file - DO NOT CHANGE
+param = VsiParseXml(fnameXml, ModeName);
+BmodeNumSamples = param.BmodeNumSamples;
+BmodeNumLines = param.BmodeNumLines;
+BmodeDepthOffset = param.BmodeDepthOffset; %mm
+BmodeDepth = param.BmodeDepth; %mm
+BmodeWidth = param.BmodeWidth; %mm
+BmodeDepth =  param.BmodeDepth; %mm
+BmodeWidth =  param.BmodeWidth; %mm
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+% This is to strip the header data in the files - DO NOT CHANGE
+size = 1; % 4 bytes
+file_header = 40; % 40bytes
+line_header = 0; % 4bytes
+frame_header = 56; % 56bytes  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fid = fopen(fname,'r');
+DepthAxis = [BmodeDepthOffset:(BmodeDepth-BmodeDepthOffset)/(BmodeNumSamples-1):BmodeDepth];
+WidthAxis = [0:BmodeWidth/(BmodeNumLines-1):BmodeWidth];
+
+% Initialize data
+Rawdata = zeros(BmodeNumSamples, BmodeNumLines);
+
+header = file_header + frame_header*iframe + (size*BmodeNumSamples*BmodeNumLines + BmodeNumLines*line_header)*(iframe-1);
+
+for i=1:BmodeNumLines
+
+    fseek(fid, header + (size*BmodeNumSamples + line_header)*(i-1),-1);
+    fseek(fid, line_header, 'cof');
+    [Rawdata(:,i),count]=fread(fid, BmodeNumSamples, 'uchar');
+    
+end
+
+fclose(fid);
---
+++ spm/pat12/Vevo_LAZR/VsiShowRawBmode8.m
@@ -0,0 +1,30 @@
+% VsiShowRawBmode8.m
+% Copyright VisualSonics 1999-2012
+% A. Needles
+% Revision: 1.0 Oct 24 2012
+% A script to display RAW 8-bit B-Mode files from data export on the Vevo 2100
+% and calls the functions "VsiOpenRawBmode8" and "VsiParseXml"
+
+
+clear all
+close all
+
+% Edit these parameters
+fbase = 'Bmode8.raw';
+fmode = '.bmode';
+StartFrame = 1;
+EndFrame = 1;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for iframe = StartFrame:EndFrame
+    
+    [ImageData, WidthAxis, DepthAxis] = VsiOpenRawBmode8(fbase, fmode, iframe);
+	
+    figure; imagesc(WidthAxis, DepthAxis, ImageData);
+	colormap('gray')
+	colorbar
+	axis equal
+	axis tight
+    xlabel('(mm)')
+    ylabel('(mm)')
+end
