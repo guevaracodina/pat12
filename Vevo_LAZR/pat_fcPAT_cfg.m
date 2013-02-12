7b76654d875c2e7991c49673d6f768caaf8eadcf
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2013-02-12 00:27:16 +0000

    //EGC Temporal filtering and GLM regression modules added. Work in progress...

--- Vevo_LAZR/pat_fcPAT_cfg.m
+++ Vevo_LAZR/pat_fcPAT_cfg.m
@@ -1,6 +1,6 @@
 function fcPAT = pat_fcPAT_cfg
 % Graphical interface configuration function for functional connectivity mapping
-% with photoacoustic tomography (fcPAT) module
+% based on photoacoustic tomography (fcPAT) signals.
 %_______________________________________________________________________________
 % Copyright (C) 2012 LIOM Laboratoire d'Imagerie Optique et Moléculaire
 %                    École Polytechnique de Montréal
@@ -10,12 +10,11 @@ fcPAT        = cfg_choice;
 fcPAT.name   = 'Functional connectivity mapping (fcPAT)';
 fcPAT.tag    = 'fcPAT';
 fcPAT.values = {pat_raw_pamode_read_cfg pat_raw_bmode_read_cfg pat_brainmask_cfg...
-    pat_create_roi_cfg pat_spatial_LPF_cfg pat_extract_roi_time_series_cfg};
-% fcPAT.values = {pat_raw_pamode_read_cfg pat_raw_bmode_read_cfg pat_brainmask_cfg...
-%     pat_create_roi_cfg pat_spatial_LPF_cfg...
-%     pat_extract_roi_time_series_cfg pat_filtdown_cfg pat_fc_GLM_on_ROI_cfg ...
+    pat_create_roi_cfg pat_spatial_LPF_cfg pat_extract_roi_time_series_cfg ...
+    pat_filtdown_cfg pat_fc_GLM_on_ROI_cfg};
+% fcPAT.values = {
 %     pat_correlation_map_cfg pat_group_corr_cfg pat_update_elinfo_cfg ...
 %     pat_get_elinfo_data_cfg pat_fcIOS_maps_cfg};
-fcPAT.help   = {'These modules perform resting-state functional connectivity mapping with photoacoustic tomography (fcPAT).'};
+fcPAT.help   = {'These modules perform resting-state functional connectivity mapping based on photoacoustic tomography (fcPAT) signals.'};
 
 % EOF
