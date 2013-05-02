8bbc96f895a3e7cbbd653bc7aa51f91adbe6cabe
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2013-05-02 20:42:51 +0000

    // EGC Exploration of global signal regression (scripts)

--- spm/pat12/Vevo_LAZR/script_pat_partial_correlations_noregress.m
+++ spm/pat12/Vevo_LAZR/script_pat_partial_correlations_noregress.m
@@ -1,14 +1,14 @@
 % script_pat_partial_correlations
-ctrl = nan([3 9]);
-lps =  nan([3 9]);
+ctrlNoRegress = nan([3 9]);
+lpsNoRegress =  nan([3 9]);
 c1 = 2;
 INTERP = true;
 clc;
-fprintf('Bilateral correlation with global signal regression\n')
+fprintf('Bilateral correlation without global signal regression\n')
 
 %% 2012-09-07-14-48-55_ctl02 7:8, 175:180, 192:200
 load('E:\Edgar\Data\PAT_Results\2012-09-07-14-48-55_ctl02\seedRadius14\BPF\PAT.mat')
-load(PAT.fcPAT.SPM.fnameROIregress)
+load(PAT.fcPAT.filtNdown.fname)
 % scan name
 [~, ~, ~, ~, ~, ~, splitStr] = regexp(PAT.input_dir,'\\');
 TR = PAT.fcPAT.filtNdown.TR;
@@ -16,7 +16,7 @@ TR = PAT.fcPAT.filtNdown.TR;
 idx = 9:174;
 % interpolation
 if INTERP
-    N = numel(ROIregress{1}{c1});
+    N = numel(filtNdownROI{1}{c1});
     xi = 1:N;
     x = [1:6, 9:174, 181:191, 201:N];
     idx = 1:N;
@@ -25,20 +25,20 @@ fprintf('%s, correlation computed for ~ %.0f s.\n',splitStr{end-1}, TR*numel(idx
 for iROI=1:2:18,
     if INTERP
         % ROI odd
-        y = ROIregress{iROI}{c1}(x);
+        y = filtNdownROI{iROI}{c1}(x);
         yi = interp1(x, y, xi);
-        ROIregress{iROI}{c1} = yi;
+        filtNdownROI{iROI}{c1} = yi;
         % ROI even
-        y = ROIregress{iROI+1}{c1}(x);
+        y = filtNdownROI{iROI+1}{c1}(x);
         yi = interp1(x, y, xi);
-        ROIregress{iROI+1}{c1} = yi;
+        filtNdownROI{iROI+1}{c1} = yi;
     end
-    ctrl(1, (iROI+1)/2) = corr(ROIregress{iROI}{c1}(idx)', ROIregress{iROI+1}{c1}(idx)');
+    ctrlNoRegress(1, (iROI+1)/2) = corr(filtNdownROI{iROI}{c1}(idx)', filtNdownROI{iROI+1}{c1}(idx)');
 end
 
 %% 2012-11-09-16-18-31_ctl03 122:127, 190:193
 load('E:\Edgar\Data\PAT_Results\2012-11-09-16-18-31_ctl03\seedRadius10\PAT.mat')
-load(PAT.fcPAT.SPM.fnameROIregress)
+load(PAT.fcPAT.filtNdown.fname)
 % scan name
 [~, ~, ~, ~, ~, ~, splitStr] = regexp(PAT.input_dir,'\\');
 TR = PAT.fcPAT.filtNdown.TR;
@@ -46,7 +46,7 @@ TR = PAT.fcPAT.filtNdown.TR;
 idx = 128:189;
 % interpolation
 if INTERP
-    N = numel(ROIregress{1}{c1});
+    N = numel(filtNdownROI{1}{c1});
     xi = 1:N;
     x = [1:121, 128:189, 194:N];
     idx = 1:N;
@@ -55,20 +55,20 @@ fprintf('%s, correlation computed for ~ %.0f s.\n',splitStr{end-1}, TR*numel(idx
 for iROI=1:2:18,
     if INTERP
         % ROI odd
-        y = ROIregress{iROI}{c1}(x);
+        y = filtNdownROI{iROI}{c1}(x);
         yi = interp1(x, y, xi);
-        ROIregress{iROI}{c1} = yi;
+        filtNdownROI{iROI}{c1} = yi;
         % ROI even
-        y = ROIregress{iROI+1}{c1}(x);
+        y = filtNdownROI{iROI+1}{c1}(x);
         yi = interp1(x, y, xi);
-        ROIregress{iROI+1}{c1} = yi;
+        filtNdownROI{iROI+1}{c1} = yi;
     end
-    ctrl(2, (iROI+1)/2) = corr(ROIregress{iROI}{c1}(idx)', ROIregress{iROI+1}{c1}(idx)');
+    ctrlNoRegress(2, (iROI+1)/2) = corr(filtNdownROI{iROI}{c1}(idx)', filtNdownROI{iROI+1}{c1}(idx)');
 end
 
 %% 2012-11-09-16-21-53_ctl01 194:199, 213
 load('E:\Edgar\Data\PAT_Results\2012-11-09-16-21-53_ctl01\seedRadius10\PAT.mat')
-load(PAT.fcPAT.SPM.fnameROIregress)
+load(PAT.fcPAT.filtNdown.fname)
 % scan name
 [~, ~, ~, ~, ~, ~, splitStr] = regexp(PAT.input_dir,'\\');
 TR = PAT.fcPAT.filtNdown.TR;
@@ -76,7 +76,7 @@ TR = PAT.fcPAT.filtNdown.TR;
 idx = 1:193;
 % interpolation
 if INTERP
-    N = numel(ROIregress{1}{c1});
+    N = numel(filtNdownROI{1}{c1});
     xi = 1:N;
     x = [1:193, 200:212, 214:N];
     idx = 1:N;
@@ -85,20 +85,20 @@ fprintf('%s, correlation computed for ~ %.0f s.\n',splitStr{end-1}, TR*numel(idx
 for iROI=1:2:18,
     if INTERP
         % ROI odd
-        y = ROIregress{iROI}{c1}(x);
+        y = filtNdownROI{iROI}{c1}(x);
         yi = interp1(x, y, xi);
-        ROIregress{iROI}{c1} = yi;
+        filtNdownROI{iROI}{c1} = yi;
         % ROI even
-        y = ROIregress{iROI+1}{c1}(x);
+        y = filtNdownROI{iROI+1}{c1}(x);
         yi = interp1(x, y, xi);
-        ROIregress{iROI+1}{c1} = yi;
+        filtNdownROI{iROI+1}{c1} = yi;
     end
-    ctrl(3, (iROI+1)/2) = corr(ROIregress{iROI}{c1}(idx)', ROIregress{iROI+1}{c1}(idx)');
+    ctrlNoRegress(3, (iROI+1)/2) = corr(filtNdownROI{iROI}{c1}(idx)', filtNdownROI{iROI+1}{c1}(idx)');
 end
 
 %% 2012-11-09-16-23-04_toe08 None
 load('E:\Edgar\Data\PAT_Results\2012-11-09-16-23-04_toe08\seedRadius10\PAT.mat')
-load(PAT.fcPAT.SPM.fnameROIregress)
+load(PAT.fcPAT.filtNdown.fname)
 % scan name
 [~, ~, ~, ~, ~, ~, splitStr] = regexp(PAT.input_dir,'\\');
 TR = PAT.fcPAT.filtNdown.TR;
@@ -106,7 +106,7 @@ TR = PAT.fcPAT.filtNdown.TR;
 idx = 1:222;
 % interpolation
 if INTERP
-    N = numel(ROIregress{1}{c1});
+    N = numel(filtNdownROI{1}{c1});
     xi = 1:N;
     x = 1:N;
     idx = 1:N;
@@ -115,20 +115,20 @@ fprintf('%s, correlation computed for ~ %.0f s.\n',splitStr{end-1}, TR*numel(idx
 for iROI=1:2:18,
     if INTERP
         % ROI odd
-        y = ROIregress{iROI}{c1}(x);
+        y = filtNdownROI{iROI}{c1}(x);
         yi = interp1(x, y, xi);
-        ROIregress{iROI}{c1} = yi;
+        filtNdownROI{iROI}{c1} = yi;
         % ROI even
-        y = ROIregress{iROI+1}{c1}(x);
+        y = filtNdownROI{iROI+1}{c1}(x);
         yi = interp1(x, y, xi);
-        ROIregress{iROI+1}{c1} = yi;
+        filtNdownROI{iROI+1}{c1} = yi;
     end
-    lps(1, (iROI+1)/2) = corr(ROIregress{iROI}{c1}(idx)', ROIregress{iROI+1}{c1}(idx)');
+    lpsNoRegress(1, (iROI+1)/2) = corr(filtNdownROI{iROI}{c1}(idx)', filtNdownROI{iROI+1}{c1}(idx)');
 end
 
 %% 2012-11-09-16-23-51_toe09 4:5, 50, 74:76, 107
 load('E:\Edgar\Data\PAT_Results\2012-11-09-16-23-51_toe09\seedRadius10\PAT.mat')
-load(PAT.fcPAT.SPM.fnameROIregress)
+load(PAT.fcPAT.filtNdown.fname)
 % scan name
 [~, ~, ~, ~, ~, ~, splitStr] = regexp(PAT.input_dir,'\\');
 TR = PAT.fcPAT.filtNdown.TR;
@@ -136,7 +136,7 @@ TR = PAT.fcPAT.filtNdown.TR;
 idx = 108:219;
 % interpolation
 if INTERP
-    N = numel(ROIregress{1}{c1});
+    N = numel(filtNdownROI{1}{c1});
     xi = 1:N;
     x = [1:3, 6:49, 51:73, 77:106, 108:N];
     idx = 1:N;
@@ -145,20 +145,20 @@ fprintf('%s, correlation computed for ~ %.0f s.\n',splitStr{end-1}, TR*numel(idx
 for iROI=1:2:18,
     if INTERP
         % ROI odd
-        y = ROIregress{iROI}{c1}(x);
+        y = filtNdownROI{iROI}{c1}(x);
         yi = interp1(x, y, xi);
-        ROIregress{iROI}{c1} = yi;
+        filtNdownROI{iROI}{c1} = yi;
         % ROI even
-        y = ROIregress{iROI+1}{c1}(x);
+        y = filtNdownROI{iROI+1}{c1}(x);
         yi = interp1(x, y, xi);
-        ROIregress{iROI+1}{c1} = yi;
+        filtNdownROI{iROI+1}{c1} = yi;
     end
-    lps(2, (iROI+1)/2) = corr(ROIregress{iROI}{c1}(idx)', ROIregress{iROI+1}{c1}(idx)');
+    lpsNoRegress(2, (iROI+1)/2) = corr(filtNdownROI{iROI}{c1}(idx)', filtNdownROI{iROI+1}{c1}(idx)');
 end
 
 %% 2012-11-09-16-16-25_toe04	115:116, 189:190, 221:222, 263
 load('E:\Edgar\Data\PAT_Results\2012-11-09-16-16-25_toe04\seedRadius10\PAT.mat')
-load(PAT.fcPAT.SPM.fnameROIregress)
+load(PAT.fcPAT.filtNdown.fname)
 % scan name
 [~, ~, ~, ~, ~, ~, splitStr] = regexp(PAT.input_dir,'\\');
 TR = PAT.fcPAT.filtNdown.TR;
@@ -166,7 +166,7 @@ TR = PAT.fcPAT.filtNdown.TR;
 idx = 1:114;
 % interpolation
 if INTERP
-    N = numel(ROIregress{1}{c1});
+    N = numel(filtNdownROI{1}{c1});
     xi = 1:N;
     x = [1:114, 117:188, 191:220, 223:262, 264:N];
     idx = 1:N;
@@ -175,23 +175,23 @@ fprintf('%s, correlation computed for ~ %.0f s.\n',splitStr{end-1}, TR*numel(idx
 for iROI=1:2:18,
     if INTERP
         % ROI odd
-        y = ROIregress{iROI}{c1}(x);
+        y = filtNdownROI{iROI}{c1}(x);
         yi = interp1(x, y, xi);
-        ROIregress{iROI}{c1} = yi;
+        filtNdownROI{iROI}{c1} = yi;
         % ROI even
-        y = ROIregress{iROI+1}{c1}(x);
+        y = filtNdownROI{iROI+1}{c1}(x);
         yi = interp1(x, y, xi);
-        ROIregress{iROI+1}{c1} = yi;
+        filtNdownROI{iROI+1}{c1} = yi;
     end
-    lps(3, (iROI+1)/2) = corr(ROIregress{iROI}{c1}(idx)', ROIregress{iROI+1}{c1}(idx)');
+    lpsNoRegress(3, (iROI+1)/2) = corr(filtNdownROI{iROI}{c1}(idx)', filtNdownROI{iROI+1}{c1}(idx)');
 end
 
 
 %% Stats
-mCtrl = mean(ctrl);
-semCtrl = std(ctrl)/sqrt(size(ctrl,1));
-mLPS = mean(lps);
-semLPS = std(lps)/sqrt(size(lps,1));
+mCtrl = mean(ctrlNoRegress);
+semCtrl = std(ctrlNoRegress)/sqrt(size(ctrlNoRegress,1));
+mLPS = mean(lpsNoRegress);
+semLPS = std(lpsNoRegress)/sqrt(size(lpsNoRegress,1));
 
 %% Graph display
 close all; h = figure;
@@ -221,7 +221,7 @@ ylim([-0.4 0.9]);
 set(h, 'units', 'inches')
 set(h, 'Position', figSize); set(h, 'PaperPosition', figSize);
 dirName = 'E:\Edgar\Data\PAT_Results\PAS2013\interp6rats';
-newName = 'PAS2013_bilateral_correlation';
+newName = 'PAS2013_bilateral_correlation_noregress';
 set(h,'Name',newName);
 print(h, '-dpng', fullfile(dirName,newName), sprintf('-r%d',300));
 % Save as a figure
