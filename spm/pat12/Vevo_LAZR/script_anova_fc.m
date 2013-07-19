527d7caa5123dabb9057fd341992b4d5cd2bdc37
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2013-07-19 22:05:06 +0000

    // EGC script_locoregional_cortical_pat.m - Final results

---
+++ spm/pat12/Vevo_LAZR/script_anova_fc.m
@@ -0,0 +1,4 @@
+function script_anova_fc
+load('F:\Edgar\Data\PAT_Results_20130517\RS\group_results_HbO_HbR_LPS_Ctrl\group_corr_pair_seeds.mat')
+load('F:\Edgar\Data\PAT_Results_20130517\RS\group_results_HbO_HbR_LPS_Sham\group_corr_pair_seeds.mat')
+% EOF
