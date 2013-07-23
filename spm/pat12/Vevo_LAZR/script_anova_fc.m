9f7c59f49b3bfbad4eaa121b2b195049edecdbdc
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2013-07-23 22:51:46 +0000

    // EGC script_oxysurge_group_results.m - Added series E

--- spm/pat12/Vevo_LAZR/script_anova_fc.m
+++ spm/pat12/Vevo_LAZR/script_anova_fc.m
@@ -1,4 +1,5 @@
 function script_anova_fc
 load('F:\Edgar\Data\PAT_Results_20130517\RS\group_results_HbO_HbR_LPS_Ctrl\group_corr_pair_seeds.mat')
 load('F:\Edgar\Data\PAT_Results_20130517\RS\group_results_HbO_HbR_LPS_Sham\group_corr_pair_seeds.mat')
+group = {'LPS'; 'Control'; 'NaCl (sham)'};
 % EOF
