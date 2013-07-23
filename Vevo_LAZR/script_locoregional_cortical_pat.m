95c15dc020bd021fc360bf51c091a274542f95a6
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2013-07-23 22:51:46 +0000

    // EGC script_oxysurge_group_results.m - Added series E

--- Vevo_LAZR/script_locoregional_cortical_pat.m
+++ Vevo_LAZR/script_locoregional_cortical_pat.m
@@ -2,6 +2,21 @@
 close all; clear; clc;
 % scrubbing
 DO_SCRUBBING = true;
+% Choose ROI selection method (all/selected)
+%     'CPu_L'
+%     'CPu_R'
+%     'LV_L'
+%     'LV_R'
+%     'M_L'
+%     'M_R'
+%     'S1_L'
+%     'S1_R'
+%     'S1BF_L'
+%     'S1BF_R'
+%     'Cortex_L'
+%     'Cortex_R'
+job.ROI_choice              = [11];     % Analyze only Left Cortex (injection side)
+
 % Control group
 job.PATmatCtrl = {  
                 'F:\Edgar\Data\PAT_Results_20130517\RS\2012-09-07-12-10-31_ctl01\newROIs\PAT.mat'
@@ -41,21 +56,6 @@ job.PATmatLPS = {
 % Colors to include (OD,HbO,HbR,HbT,Flow)
 job.IC                      = pat_include_colors_cfg(1,1);
           
-% Choose ROI selection method (all/selected)
-%     'CPu_L'
-%     'CPu_R'
-%     'LV_L'
-%     'LV_R'
-%     'M_L'
-%     'M_R'
-%     'S1_L'
-%     'S1_R'
-%     'S1BF_L'
-%     'S1BF_R'
-%     'Cortex_L'
-%     'Cortex_R'
-job.ROI_choice              = [12];     % Analyze only Left Cortex (injection side)
-
 % Significance level
 job.optStat.alpha           = 0.05;
 
@@ -251,36 +251,36 @@ end
 % Data for ANOVA
 LPS = AvgLPS(:,2);
 
-%% perform 1-Way ANOVA
 % Create parent results directory if it does not exist
 if ~exist(job.parent_results_dir{1},'dir'), mkdir(job.parent_results_dir{1}); end
 % Save AvgCtrl, AvgLPS
 dataFileName = 'locoregional_SO2_data';
 save(fullfile(job.parent_results_dir{1},dataFileName),'AvgCtrl','AvgLPS',...
-    'CtrlInjected','Ctrl','LPS')
-colorNames      = fieldnames(PAT.color);
-% ANOVA
+    'CtrlInjected','Ctrl','LPS','job','PAT')
+
+%% perform 1-Way ANOVA
+% load ('F:\Edgar\Data\PAT_Results_20130517\RS\locoregional\LeftCortex\locoregional_SO2_data')
+% load ('F:\Edgar\Data\PAT_Results_20130517\RS\locoregional\RightCortex\locoregional_SO2_data')
 close all
-alphaVal = 0.05;
 % c1 is measurements index
 c1 = 1;
 criterionType = 'tukey-kramer';
-group = {'Control'; 'LPS'; 'NaCl (sham)'};
-% z_seed12_tmp = squeeze(Z(1,2,:));
+group = {'LPS'; 'Control'; 'NaCl (sham)'};
 nRows = max([size(LPS,1); size(Ctrl,1); size(CtrlInjected,1)]);
 groupedData = nan([nRows, numel(group)]);
-groupedData(1:numel(Ctrl(:,c1)), 1)         = Ctrl(:,c1);
-groupedData(1:numel(LPS(:,c1)), 2)          = LPS(:,c1);
+groupedData(1:numel(LPS(:,c1)), 1)          = LPS(:,c1);
+groupedData(1:numel(Ctrl(:,c1)), 2)         = Ctrl(:,c1);
 groupedData(1:numel(CtrlInjected(:,c1)), 3) = CtrlInjected(:,c1);
-[p1, table1, stats1] = anova1(groupedData, group);
+[p1, table1, stats1] = anova1(groupedData, group, 'off');
 h = figure;
-[comparison, means, h, groupNames] = multcompare(stats1, 'alpha', alphaVal, 'ctype', criterionType);
+[comparison, means, h, groupNames] = multcompare(stats1, 'alpha', job.optStat.alpha, 'ctype', criterionType);
 set(h,'Name','Multiple comparison of average SO_2 values');
 title('Locoregional cortical SO_2')
 disp([groupNames num2cell(comparison)]);
 
 %% Print multiple comparisons ANOVA
 if job.save_figures
+    colorNames      = fieldnames(PAT.color);
     % Specify window units
     set(h, 'units', 'inches')
     % Change figure and paper size
@@ -297,105 +297,106 @@ if job.save_figures
     close(h)
 end
 
-%% statistical test comparison
+%% statistical test comparison (to be performed for two groups only)
 
-for c1 = 1:2
-    [statTest(1).t(1).H{c1}, statTest(1).t(1).P{c1}, statTest(1).t(1).CI{c1}, ...
-        statTest(1).t(1).STATS{c1}] = ...
-        ttest2(AvgCtrl(:,c1), AvgLPS(:,c1), job.optStat.alpha,'both');
-    statTest(1).t(1).id = 'Unpaired-sample t-test';
-    fprintf('%s: Contrast (%s) p=%0.4f\n',statTest(1).t(1).id, colorNames{1+c1},statTest(1).t(1).P{c1});
-    [statTest(1).w(1).P{c1}, statTest(1).w(1).H{c1}, statTest(1).w(1).STATS{c1}] =...
-        ranksum (AvgCtrl(:,c1), AvgLPS(:,c1), job.optStat.alpha);
-    statTest(1).w(1).id = 'Wilcoxon rank sum test';
-    fprintf('%s: Contrast (%s) p=%0.4f\n', statTest(1).w(1).id, colorNames{1+c1},statTest(1).w(1).P{c1});
-end
+% for c1 = 1:2
+%     [statTest(1).t(1).H{c1}, statTest(1).t(1).P{c1}, statTest(1).t(1).CI{c1}, ...
+%         statTest(1).t(1).STATS{c1}] = ...
+%         ttest2(AvgCtrl(:,c1), AvgLPS(:,c1), job.optStat.alpha,'both');
+%     statTest(1).t(1).id = 'Unpaired-sample t-test';
+%     fprintf('%s: Contrast (%s) p=%0.4f\n',statTest(1).t(1).id, colorNames{1+c1},statTest(1).t(1).P{c1});
+%     [statTest(1).w(1).P{c1}, statTest(1).w(1).H{c1}, statTest(1).w(1).STATS{c1}] =...
+%         ranksum (AvgCtrl(:,c1), AvgLPS(:,c1), job.optStat.alpha);
+%     statTest(1).w(1).id = 'Wilcoxon rank sum test';
+%     fprintf('%s: Contrast (%s) p=%0.4f\n', statTest(1).w(1).id, colorNames{1+c1},statTest(1).w(1).P{c1});
+% end
 
-%% Plot results
-% Plots statistical analysis group results
-% Positioning factor for the * mark, depends on max data value at the given seed
-starPosFactor   = 1.05;
-% Font Sizes
-axisFontSize    = 12;
-starFontSize    = 22;
-axMargin        = 0.5;
-labelYaxis{1}   = 'HbT (a.u.)';
-labelYaxis{2}   = 'SO_2 (%)';
-statsNames = {'t' 'w'};
-statsID = {'T-test' 'Wilcoxon'};
-for iStats = 1:numel(statsNames)
-    for c1 = 1:2
-        y = [mean(AvgCtrl(:,c1)); mean(AvgLPS(:,c1))];
-        e = [std(AvgCtrl(:,c1)); std(AvgLPS(:,c1))];
-        % Show standard error bars instead of standard deviation
-        if job.optFig.stderror
-            sampleSize = ones(size(e));
-            % First row: Control group
-            sampleSize(1,:) = sampleSize(1,:) .* numel(AvgCtrl(:,c1));
-            % Second row: Treatment group
-            sampleSize(2,:) = sampleSize(2,:) .* numel(AvgLPS(:,c1));
-            % std error bars: sigma/sqrt(N)
-            e = e ./ sqrt(sampleSize);
-        end
-        if job.generate_figures
-            % Display plots on new figure
-            h = figure; set(gcf,'color','w')
-            % Specify window units
-            set(h, 'units', 'inches')
-            % Change figure and paper size
-            set(h, 'Position', [0.1 0.1 job.optFig.figSize(1) job.optFig.figSize(2)])
-            set(h, 'PaperPosition', [0.1 0.1 job.optFig.figSize(1) job.optFig.figSize(2)])
-            % Custom bar graphs with error bars (1st arg: error)
-            barwitherr(e, y)
-            % Display colormap according to the contrast
-            switch(c1)
-                case 1
-                    % HbT contrast
-                    colormap([0.5 0.5 0.5; 1 1 1]);
-                case 2
-                    % SO2 contrast
-                    colormap([0.25 0.25 0.25; 1 1 1]);
-                case 3
-                    % B-mode contrast
-                    colormap([0 0 0; 1 1 1]);
-                otherwise
-                    colormap(gray)
-            end
-            
-            title(sprintf('C%d(%s) %s (*p<%.2g)',...
-                c1,colorNames{1+c1}, statsID{iStats}, job.optStat.alpha),'interpreter','none','FontSize',job.optFig.titleFontSize)
-            set(gca,'FontSize',axisFontSize)
-            ylabel(labelYaxis{c1},'FontSize',job.optFig.yLabelFontSize)
-            set(gca,'XTickLabel',job.optFig.xAxisLabels,'FontWeight', 'b','FontSize',job.optFig.xLabelFontSize)
-            if isfield(job.optFig.legends, 'legendShow')
-                legend(job.optFig.legends.legendShow.legendStr,'FontSize',job.optFig.legends.legendShow.legendFontSize,'location',job.optFig.legends.legendShow.legendLocation)
-            end
-            set(gca, 'xLim', [axMargin size(y,1) + axMargin]);
-            if isfield(job.optFig.yLimits, 'yLimManual')
-                set(gca, 'ylim', job.optFig.yLimits.yLimManual.yLimValue)
-            end
-            % Show a * when a significant difference is found.
-            if statTest(1).(statsNames{iStats})(1).H{c1}
-                if max(e)>=0
-                    yPos = starPosFactor*(max(y(:)) + max(e(:)));
-                else
-                    yPos = starPosFactor*(min(y(:)) - max(e(:)));
-                end
-                xPos = 1;
-                text(xPos, yPos, '*', 'FontSize', starFontSize, 'FontWeight', 'b');
-            end
-            if job.save_figures
-                newName = sprintf('groupCorr_%c_C%d_(%s)',statsNames{iStats},c1,colorNames{1+c1});
-                % Save as PNG
-                print(h, '-dpng', fullfile(job.parent_results_dir{1},newName), sprintf('-r%d',job.optFig.figRes));
-                % Save as a figure
-                saveas(h, fullfile(job.parent_results_dir{1},newName), 'fig');
-                % Return the property to its default
-                set(h, 'units', 'pixels')
-                close(h)
-            end
-        end % end generate figures
-    end % colors loop
-end % stats loop
+%% Plot results %% statistical test comparison (for two groups only)
+
+% % Plots statistical analysis group results
+% % Positioning factor for the * mark, depends on max data value at the given seed
+% starPosFactor   = 1.05;
+% % Font Sizes
+% axisFontSize    = 12;
+% starFontSize    = 22;
+% axMargin        = 0.5;
+% labelYaxis{1}   = 'HbT (a.u.)';
+% labelYaxis{2}   = 'SO_2 (%)';
+% statsNames = {'t' 'w'};
+% statsID = {'T-test' 'Wilcoxon'};
+% for iStats = 1:numel(statsNames)
+%     for c1 = 1:2
+%         y = [mean(AvgCtrl(:,c1)); mean(AvgLPS(:,c1))];
+%         e = [std(AvgCtrl(:,c1)); std(AvgLPS(:,c1))];
+%         % Show standard error bars instead of standard deviation
+%         if job.optFig.stderror
+%             sampleSize = ones(size(e));
+%             % First row: Control group
+%             sampleSize(1,:) = sampleSize(1,:) .* numel(AvgCtrl(:,c1));
+%             % Second row: Treatment group
+%             sampleSize(2,:) = sampleSize(2,:) .* numel(AvgLPS(:,c1));
+%             % std error bars: sigma/sqrt(N)
+%             e = e ./ sqrt(sampleSize);
+%         end
+%         if job.generate_figures
+%             % Display plots on new figure
+%             h = figure; set(gcf,'color','w')
+%             % Specify window units
+%             set(h, 'units', 'inches')
+%             % Change figure and paper size
+%             set(h, 'Position', [0.1 0.1 job.optFig.figSize(1) job.optFig.figSize(2)])
+%             set(h, 'PaperPosition', [0.1 0.1 job.optFig.figSize(1) job.optFig.figSize(2)])
+%             % Custom bar graphs with error bars (1st arg: error)
+%             barwitherr(e, y)
+%             % Display colormap according to the contrast
+%             switch(c1)
+%                 case 1
+%                     % HbT contrast
+%                     colormap([0.5 0.5 0.5; 1 1 1]);
+%                 case 2
+%                     % SO2 contrast
+%                     colormap([0.25 0.25 0.25; 1 1 1]);
+%                 case 3
+%                     % B-mode contrast
+%                     colormap([0 0 0; 1 1 1]);
+%                 otherwise
+%                     colormap(gray)
+%             end
+%             
+%             title(sprintf('C%d(%s) %s (*p<%.2g)',...
+%                 c1,colorNames{1+c1}, statsID{iStats}, job.optStat.alpha),'interpreter','none','FontSize',job.optFig.titleFontSize)
+%             set(gca,'FontSize',axisFontSize)
+%             ylabel(labelYaxis{c1},'FontSize',job.optFig.yLabelFontSize)
+%             set(gca,'XTickLabel',job.optFig.xAxisLabels,'FontWeight', 'b','FontSize',job.optFig.xLabelFontSize)
+%             if isfield(job.optFig.legends, 'legendShow')
+%                 legend(job.optFig.legends.legendShow.legendStr,'FontSize',job.optFig.legends.legendShow.legendFontSize,'location',job.optFig.legends.legendShow.legendLocation)
+%             end
+%             set(gca, 'xLim', [axMargin size(y,1) + axMargin]);
+%             if isfield(job.optFig.yLimits, 'yLimManual')
+%                 set(gca, 'ylim', job.optFig.yLimits.yLimManual.yLimValue)
+%             end
+%             % Show a * when a significant difference is found.
+%             if statTest(1).(statsNames{iStats})(1).H{c1}
+%                 if max(e)>=0
+%                     yPos = starPosFactor*(max(y(:)) + max(e(:)));
+%                 else
+%                     yPos = starPosFactor*(min(y(:)) - max(e(:)));
+%                 end
+%                 xPos = 1;
+%                 text(xPos, yPos, '*', 'FontSize', starFontSize, 'FontWeight', 'b');
+%             end
+%             if job.save_figures
+%                 newName = sprintf('groupCorr_%c_C%d_(%s)',statsNames{iStats},c1,colorNames{1+c1});
+%                 % Save as PNG
+%                 print(h, '-dpng', fullfile(job.parent_results_dir{1},newName), sprintf('-r%d',job.optFig.figRes));
+%                 % Save as a figure
+%                 saveas(h, fullfile(job.parent_results_dir{1},newName), 'fig');
+%                 % Return the property to its default
+%                 set(h, 'units', 'pixels')
+%                 close(h)
+%             end
+%         end % end generate figures
+%     end % colors loop
+% end % stats loop
         
 % EOF
--- Vevo_LAZR/script_oxysurge_group_results.m
+++ Vevo_LAZR/script_oxysurge_group_results.m
@@ -1,16 +1,33 @@
 %% script_oxysurge_group_results
-load('D:\Edgar\Documents\Dropbox\Docs\PAT\OS\TablResultat.mat')
-Data = cell2mat(TablResultat(2:end,2:end));
-% CtrlIdx = [4 7 8 10 12 13 15 16];
+clear; close all; clc;
+load('F:\Edgar\Data\PAT_Results_20130517\OxySurge\TablResultatSerieE.mat')
+% Rename serie E variable
+TablResultatSerieE = TablResultat; clear TablResultat;
+% Load data from first 2 series
+load('F:\Edgar\Data\PAT_Results_20130517\OxySurge\TablResultat.mat')
+% Animals ID
+ID = [TablResultat(2:end,1); TablResultatSerieE(2:end,1)];
+% Grouped data
+Data = cell2mat([TablResultat(2:end,2:end); TablResultatSerieE(2:end,2:end)]);
+
+% First series only
 CtrlIdx = [10 13 15 16];
-CtrlInjectedIdx = [7 8 12];
-LPSIdx = [1 2 3 5 6 9 11 14];
+CtrlInjectedIdx = [7 8 12 ];
+LPSIdx = [1 2 3 5 6 9 11 14 ];
+
+% Whole series
+% CtrlIdx = [10 13 15 16 26 27];
+% CtrlInjectedIdx = [7 8 12 17 18 19];
+% LPSIdx = [1 2 3 5 6 9 11 14 20 21 22 23 24];
+
+% Gathering data in the corresponding vectors
 Ctrl = Data(CtrlIdx,:);
 LPS = Data(LPSIdx,:);
 CtrlInjected = Data(CtrlInjectedIdx,:);
+% Significance value
 job.optStat.alpha = 0.05;
 
-%% Statistical test
+%% Statistical test (only to be performed for two groups analysis)
 clc;
 colorNames = TablResultat(1,:);
 statTest(1).w(1).id = 'Wilcoxon rank sum test';
@@ -18,42 +35,52 @@ fprintf('%s\n',statTest(1).w(1).id);
 % c1 is measurements index
 for c1 = 1:4
     [statTest(1).w(1).P{c1}, statTest(1).w(1).H{c1}, statTest(1).w(1).STATS{c1}] =...
-        ranksum (Ctrl(:,c1), LPS(:,c1), job.optStat.alpha);
+        ranksum ([Ctrl(:,c1); CtrlInjected(:,c1)], LPS(:,c1), job.optStat.alpha);
     statTest(1).w(1).id = 'Wilcoxon rank sum test';
     fprintf('(%s) Ctrl = %0.4f ± %0.4f; LPS = %0.4f ± %0.4f. p=%0.4f\n', ...
-        colorNames{1+c1}, mean(Ctrl(:,c1)), std(Ctrl(:,c1)),...
+        colorNames{1+c1}, mean([Ctrl(:,c1); CtrlInjected(:,c1)] ), std([Ctrl(:,c1); CtrlInjected(:,c1)]),...
         mean(LPS(:,c1)), std(LPS(:,c1)), statTest(1).w(1).P{c1});
-    
-    [statTest(1).w(1).P{c1}, statTest(1).w(1).H{c1}, statTest(1).w(1).STATS{c1}] =...
-        ranksum (Ctrl(:,c1), CtrlInjected(:,c1), job.optStat.alpha);
-    statTest(1).w(1).id = 'Wilcoxon rank sum test';
-    fprintf('(%s) Ctrl = %0.4f ± %0.4f; CtrlInjected = %0.4f ± %0.4f. p=%0.4f\n', ...
-        colorNames{1+c1}, mean(Ctrl(:,c1)), std(Ctrl(:,c1)),...
-        mean(LPS(:,c1)), std(LPS(:,c1)), statTest(1).w(1).P{c1});
-    
-    [statTest(1).w(1).P{c1}, statTest(1).w(1).H{c1}, statTest(1).w(1).STATS{c1}] =...
-        ranksum (LPS(:,c1), CtrlInjected(:,c1), job.optStat.alpha);
-    statTest(1).w(1).id = 'Wilcoxon rank sum test';
-    fprintf('(%s) LPS = %0.4f ± %0.4f; CtrlInjected = %0.4f ± %0.4f. p=%0.4f\n\n', ...
-        colorNames{1+c1}, mean(LPS(:,c1)), std(LPS(:,c1)),...
-        mean(CtrlInjected(:,c1)), std(CtrlInjected(:,c1)), statTest(1).w(1).P{c1});
 end
+fprintf('p = [%0.4f %0.4f %0.4f %0.4f] (FDR corrected)\n',pat_fdr(cell2mat(statTest.w.P)));
+fprintf('p = [%0.4f %0.4f %0.4f %0.4f] (uncorrected)\n',cell2mat(statTest.w.P));
 
 %% ANOVA
 close all
-alphaVal = 0.05;
-% c1 is measurements index
-c1 = 1;
-criterionType = 'tukey-kramer';
-group = {'Control'; 'LPS'; 'Control Injected'};
-% z_seed12_tmp = squeeze(Z(1,2,:));
-nRows = max([size(LPS,1); size(Ctrl,1); size(CtrlInjected,1)]);
-groupedData = nan([nRows, numel(group)]);
-groupedData(1:numel(Ctrl(:,c1)), 1) = Ctrl(:,c1);
-groupedData(1:numel(LPS(:,c1)), 2) = LPS(:,c1);
-groupedData(1:numel(CtrlInjected(:,c1)), 3) = CtrlInjected(:,c1);
-[p1,table1,stats1] = anova1(groupedData, group);
-figure;
-[comparison, means, h, groupNames] = multcompare(stats1, 'alpha', alphaVal, 'ctype', criterionType);
-disp([groupNames num2cell(comparison)]);
+alphaVal = job.optStat.alpha;
+% c1 is measurements index 
+% 1: Area under the curve
+% 2: Pos. slope
+% 3: Neg. slope
+% 4: Plateau
+for c1 = 1:4;
+    criterionType = 'tukey-kramer';
+    groupID = {'LPS'; 'Control'; 'NaCl (sham)'};
+    nRows = max([size(LPS,1); size(Ctrl,1); size(CtrlInjected,1)]);
+    % Preallocate variable with grouped data
+    groupedData = nan([nRows, numel(groupID)]);
+    % Fill grouped data variable
+    groupedData(1:numel(LPS(:,c1)), 1) = LPS(:,c1);
+    groupedData(1:numel(Ctrl(:,c1)), 2) = Ctrl(:,c1);
+    groupedData(1:numel(CtrlInjected(:,c1)), 3) = CtrlInjected(:,c1);
+    % Perform 1-way ANOVA
+    [p1,table1,stats1] = anova1(groupedData, groupID, 'off');
+    h = figure;
+    % Perform multiple-comparisons test
+    [comparison, means, h, groupNames] = multcompare(stats1, 'alpha', alphaVal, 'ctype', criterionType);
+    switch c1
+        case 1
+            windowName = 'Area under the curve';
+        case 2
+            windowName = 'Positive slope';
+        case 3
+            windowName = 'Negative Slope';
+        case 4
+            windowName = 'Plateau';
+        otherwise
+            windowName = '';
+    end
+    set(h,'Name',windowName)
+    title(windowName);
+    disp([groupNames num2cell(comparison)]);
+end
 % EOF
