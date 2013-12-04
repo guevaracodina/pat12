c0001856ee71b12da77b89946181885de0b3d877
liom <liom@a7bd82cd-e2e3-4066-afa6-8de3f1b37371>
2013-12-04 21:59:14 +0000

    // EGC pat_raw2nifti.m - Added comments script_nano_test.m, script_overlay_blend_wiki.m - Minor changes

---
+++ spm/pat12/Vevo_LAZR/script_overlay_blend_wiki.m
@@ -0,0 +1,25 @@
+%% Load data
+clear all; close all; clc
+anatVol = spm_vol('D:\Edgar\Data\IOS_Carotid_Res\12_10_18,NC09\S01\12_10_18,NC09_anat_S01.nii');
+brainVol = spm_vol('D:\Edgar\Data\IOS_Carotid_Res\12_10_18,NC09\S01\12_10_18,NC09_anat_brainmask.nii');
+fcMapVol = spm_vol('D:\Edgar\Data\IOS_Carotid_Res\alignment\HbO\NC_R08C05\AVG_fcMap.img');
+anatomical = spm_read_vols(anatVol);
+fcMap = spm_read_vols(fcMapVol);
+brainMask = spm_read_vols(brainVol);
+fcMapRange = [-0.2733 0.9052];
+alphaRange = [0.15 1];
+fcColorMap = jet(256);
+figIntensity = 1;
+% Transpose
+fcMap = fliplr(fcMap);
+fcMap = fcMap';
+brainMask = brainMask';
+anatomical = anatomical';
+
+%% Open figures
+figure; imagesc(brainMask); colormap(gray); colorbar
+figure; imagesc(anatomical); colormap(gray); colorbar
+figure; imagesc(fcMap, fcMapRange); colormap(fcColorMap); colorbar
+[fcMapBlend h] = pat_overlay_blend(anatomical, fcMap, brainMask, fcMapRange,alphaRange, fcColorMap, figIntensity);
+
+% EOF
